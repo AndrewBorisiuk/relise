7b12640bac818f89ae666ff884339f7941e2df84
Oleksandr <sashapylypenko.pov@gmail.com>
2025-11-30 20:04:57 +0200

    fix: small fix

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("привіт зі світу ігор")
+
+    const tabLinks = document.querySelectorAll('.tab-link'); //Доступ до елементів сторінки
+    const tabContents = document.querySelectorAll('.animal-block'); //Доступ до елементів сторінки
+
+    tabLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            const target = this.getAttribute('data-target');
+
+            // Пройтись по кожному блоку вмісту
+            // перевірити чи id === target
+            // якщо так, додати клас active
+            //інакше видалити клас active
+        });
+    });
