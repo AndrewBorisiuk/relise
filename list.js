9e93a77b5b7dc73f4fb9cb4ed3851afc1e260f10
AndrewBorisiuk <dmitriy.borysiuk@gmail.com>
2025-12-07 19:06:49 +0200

    finishing working with buttons

--- list.js
+++ list.js
@@ -4,13 +4,15 @@ const items = document.querySelectorAll('.category-card');
 filterButtons.forEach((button) => {
   button.addEventListener('click', () => {
     // Отримати тип фільтра з атрибута data-type
+    let type = button.getAttribute('data-type');
 
     items.forEach((item) => {
       // Порівняти тип фільтра з атрибутом data-category елемента
-      if (true) {
+      if (item.getAttribute('data-category') === type || type === 'all') {
         item.style.display = 'flex';
       } else {
         // Сховати елемент
+        item.style.display = 'none';
       }
     });
   });
