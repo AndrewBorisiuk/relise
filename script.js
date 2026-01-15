79fef8b70280a65a9e74a91421eb5a014f486d8b
AndrewBorisiuk <dmitriy.borysiuk@gmail.com>
2026-01-15 16:19:40 +0200

    checking user name

--- script.js
+++ script.js
@@ -1,53 +1,55 @@
+const tabLinks = document.querySelectorAll('.tab-link'); //Доступ до елементів сторінки
+const tabContents = document.querySelectorAll('.animal-block'); //Доступ до елементів сторінки
 
-    const tabLinks = document.querySelectorAll('.tab-link'); //Доступ до елементів сторінки
-    const tabContents = document.querySelectorAll('.animal-block'); //Доступ до елементів сторінки
-
-    tabLinks.forEach(link => {
-        link.addEventListener('click', function() {
-            const target = this.getAttribute('data-target');
-            tabContents.forEach(content => {
-                if(content.id===target){
-                    content.classList.add("active")
-                }
-                else{
-                    content.classList.remove("active")
-                }
-            });
-            // Пройтись по кожному блоку вмісту
-            // перевірити чи id === target
-            // якщо так, додати клас active
-            //інакше видалити клас active
-        });
+tabLinks.forEach((link) => {
+  link.addEventListener('click', function () {
+    const target = this.getAttribute('data-target');
+    tabContents.forEach((content) => {
+      if (content.id === target) {
+        content.classList.add('active');
+      } else {
+        content.classList.remove('active');
+      }
     });
-
-    let hero = document.querySelector(".main-image");
-  
-anime({
-    targets: [hero],
-    opacity: [0, 1],
-    translateY: [20, 0],
-    easing: 'easeOutQuad',
-    duration: 5000,
-    delay: anime.stagger(200),
+    // Пройтись по кожному блоку вмісту
+    // перевірити чи id === target
+    // якщо так, додати клас active
+    //інакше видалити клас active
   });
+});
 
-  //Перевірка чи є імя в кукі файлі
-  
+let hero = document.querySelector('.main-image');
+
+anime({
+  targets: [hero],
+  opacity: [0, 1],
+  translateY: [20, 0],
+  easing: 'easeOutQuad',
+  duration: 5000,
+  delay: anime.stagger(200),
+});
+
+//Перевірка чи є імя в кукі файлі
 
 let savedName = '';
 let isCookieSaved = false;
- let cookiesList = document.cookie.split('; ');
+let cookiesList = document.cookie.split('; ');
 
- console.log(document.cookie, 'document.cookie')
-    for(let i = 0; i < cookiesList.length; i++){
-        let name_and_value =  cookiesList[i].split('=');
-        if (name_and_value[0] == 'user-name') {
-            isCookieSaved = true;
-            savedName = name_and_value[1];
-        }
-    };
+console.log(document.cookie, 'document.cookie');
+for (let i = 0; i < cookiesList.length; i++) {
+  let name_and_value = cookiesList[i].split('=');
+  if (name_and_value[0] == 'user-name') {
+    isCookieSaved = true;
+    savedName = name_and_value[1];
+  }
+}
+if (isCookieSaved) {
+  alert('Вітаємо знову, ' + decodeURIComponent(savedName) + '!');
+} else {
+  let name = prompt('Скажіть нам ваше імя!');
+  document.cookie = `user-name=${name} ;max-age=100000000`;
+}
 
-    //Перевіряємо чи знайшли імя
-    // Якщо є, привітати користувача з іменем
-  // Якщо ні, запитати імя користувача і зберегти його в кукі файл
-  
+//Перевіряємо чи знайшли імя
+// Якщо є, привітати користувача з іменем
+// Якщо ні, запитати імя користувача і зберегти його в кукі файл
